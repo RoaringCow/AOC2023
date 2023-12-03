257f4b38af537d7f0cbbb7799b1e2f4b41df58d8
roaringcow <99341198+RoaringCow@users.noreply.github.com>
2023-12-03 14:18:22 +0300

    Delete day3/target directory

--- day3/target/debug/deps/day3-21edc8568dea9b64.d
+++
@@ -1,5 +0,0 @@
-/home/ersan/AOC2023/day3/target/debug/deps/day3-21edc8568dea9b64.rmeta: src/main.rs
-
-/home/ersan/AOC2023/day3/target/debug/deps/day3-21edc8568dea9b64.d: src/main.rs
-
-src/main.rs:
--- day3/target/release/day3.d
+++
@@ -1,1 +0,0 @@
-/home/ersan/AOC2023/day3/target/release/day3: /home/ersan/AOC2023/day3/src/main.rs
