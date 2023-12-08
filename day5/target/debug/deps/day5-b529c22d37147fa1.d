2acb7cea426d956baf55f8e2e2c83c1864cbe654
roaringcow <99341198+RoaringCow@users.noreply.github.com>
2023-12-08 22:26:37 +0300

    Delete day5/target directory

--- day5/target/debug/deps/day5-b529c22d37147fa1.d
+++
@@ -1,5 +0,0 @@
-/home/ersan/AOC2023/day5/target/debug/deps/day5-b529c22d37147fa1.rmeta: src/main.rs
-
-/home/ersan/AOC2023/day5/target/debug/deps/day5-b529c22d37147fa1.d: src/main.rs
-
-src/main.rs:
